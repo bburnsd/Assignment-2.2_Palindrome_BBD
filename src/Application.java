54ae91a8e94bb96919a7f3f03e22ae4009743ae6
bburnsd <101883596+bburnsd@users.noreply.github.com>
2022-09-29 03:26:53 -0400

    Add files via upload

---
+++ src/Application.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+import java.util.Scanner;
+/**
+ * @author Brad Davis
+ * @version 9/22/2022
+ * This application class uses a stack to test and see if the string is a palindrome
+ */
+public class Application {
+	public static void main(String[] args) {
+Palindrome s1 = new Palindrome("Race Car"); //Setting up the palindrome object with an actual palindrome, one capital letter and a space to make sure that it still confirms that it is a palindrome
+System.out.println("Test 1 | String = " + s1.getPalindromeString() +". isPalindrome = "+ s1.isPalindrome()); //checking to see if the string is a palindrome
+s1.setPalindromeString("brad");// setting the string to a word that isn't a palindrome
+System.out.println("Test 2 | String = "+ s1.getPalindromeString() +". isPalindrome = "+ s1.isPalindrome()); //checking to make sure that it returns false since it isn't a palindrome
+}
+}
+
+
+
---
+++ src/Palindrome.java
@@ -0,0 +1,70 @@
+/**
+ * @author Brad Davis
+ * A class where a user can input their own string and test to see if it is a palindrome
+ */
+import java.util.Stack;
+public class Palindrome {
+private Stack<Character> palindromestack = new Stack<Character>();
+private String palindromestring;
+private boolean palindromeCheck;
+
+/**
+ * Default constructor that creates an empty palindrome
+ */
+public Palindrome() {
+	this.palindromestring="";
+}//end default constructor
+
+/**
+ * Preferred constructor that creates a palindrome with the string the user inputs, then puts each character of that string onto a stack
+ * @param palindrome  The string the user wants to test and see is a palindrome
+ */
+public Palindrome(String palindrome) {
+	this.palindromestring=palindrome;
+	this.palindromestack.clear();
+	this.palindromestring = palindromestring.toLowerCase();
+	this.palindromestring.replaceAll("\\s", "");
+	
+	for(int i=0;i<this.palindromestring.length();i++) {
+		this.palindromestack.push(this.palindromestring.charAt(i));
+	}
+}//end preferred constructor
+
+/**
+ * Method that checks if the inputed string is a palindrome
+ * @return True if it is a palindrome, false if not
+ */
+public boolean isPalindrome() {
+	for(int i=0;i<this.palindromestring.length();i++) {
+		this.palindromeCheck = true;
+		if(this.palindromestring.charAt(i) != this.palindromestack.pop()) {
+			this.palindromeCheck = false;
+			continue;
+		}
+	}
+	return this.palindromeCheck;
+}//end isPalindrome
+
+/**
+ * get method for the palindrome string
+ * @return  returns the current string in the palindrome object
+ */
+public String getPalindromeString() {
+	return palindromestring;
+}//end getPalindromeString
+
+/**
+ * method to change the current string of the palindrome object
+ * @param palindromestring  what the user wants the new string for the palindrome to be
+ */
+public void setPalindromeString(String palindromestring) {
+	this.palindromestring = palindromestring;
+	this.palindromestack.clear();
+	this.palindromestring = palindromestring.toLowerCase();
+	this.palindromestring.replaceAll("\\s", "");
+	
+	for(int i=0;i<this.palindromestring.length();i++) {
+		this.palindromestack.push(this.palindromestring.charAt(i));
+	}
+}//end setPalindromeString
+}//end Palindrome
